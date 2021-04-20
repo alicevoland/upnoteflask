c69f16e299d38c0d63e1257d24c88b53b25b060a
Mathieu Voland <mathieu.voland@gmail.com>
2021-04-20 22:16:17 +0200

    Fix database name for heroku

--- upnote/config.py
+++ upnote/config.py
@@ -8,4 +8,9 @@ class Config(object):
     SECRET_KEY = environ.get('SECRET_KEY')
 
     SQLALCHEMY_DATABASE_URI = environ['DATABASE_URL']
+    # Fix heroku naming
+    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
